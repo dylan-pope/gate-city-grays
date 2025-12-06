0cd7f18996512235b705b9b43ff9ef86797ab2d6
Dylan Pope <152105566+dylan-pope@users.noreply.github.com>
2025-12-05 17:31:33 -0800

    changed logo size

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
       <img
         src="/images/grays-logo.png"
         alt="Gate City Grays logo"
-        className="h-20 w-20 object-contain"
+        className="h-14 w-14 object-contain"
       />
     </div>
   );
